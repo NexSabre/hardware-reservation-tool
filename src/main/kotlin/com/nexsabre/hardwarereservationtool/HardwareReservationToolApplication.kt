0d19a993b9795a82f0b723e5657c01705d2b6d3a
Nex Sabre <NexSabre@users.noreply.github.com>
2020-10-17 17:15:47 +0200

    Initial commit

---
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/HardwareReservationToolApplication.kt
@@ -0,0 +1,11 @@
+package com.nexsabre.hardwarereservationtool
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HardwareReservationToolApplication
+
+fun main(args: Array<String>) {
+	runApplication<HardwareReservationToolApplication>(*args)
+}
---
+++ src/test/kotlin/com/nexsabre/hardwarereservationtool/HardwareReservationToolApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.nexsabre.hardwarereservationtool
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HardwareReservationToolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
