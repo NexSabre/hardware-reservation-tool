9939135cb2ea2fdab51878087c6fed4abf45db8b
Nex Sabre <NexSabre@users.noreply.github.com>
2020-10-21 20:55:15 +0200

    WIP:

--- src/test/kotlin/end2end/End2EndTest.kt
+++ src/test/kotlin/end2end/End2EndTest.kt
@@ -1,14 +1,40 @@
 package end2end
 
-import makeRequest
-import org.assertj.core.api.Assertions.assertThat
+import com.nexsabre.hardwarereservationtool.models.Element
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import makeGetRequest
+import makePostRequest
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+
+
+fun countElementsOn(endpoint: String): Int = Json.decodeFromString<List<Element>>(makeGetRequest(endpoint = endpoint)).size
 
 
 class End2EndTest {
     @Test
     fun `Make request to the api_v1_reservations`() {
-        val response = makeRequest(endpoint = "/api/v1/reservations")
-        assertThat(response).isEqualTo("[]")
+        val response = makeGetRequest(endpoint = "/api/v1/reservations")
+//        assertThat(response).isEqualTo("[]")
+    }
+
+    @Test
+    fun `Add a new machine`() {
+        val endpoint = "/api/v1/machines"
+        val responseCountAllMachinesBefore = countElementsOn(endpoint)
+
+        addNewMachineThruAPI()
+
+        val responseCountAllMachinesAfter = countElementsOn(endpoint)
+        Assertions.assertThat(responseCountAllMachinesAfter).isGreaterThan(responseCountAllMachinesBefore)
+    }
+
+    fun addNewMachineThruAPI() {
+        val randomMachine = kotlin.random.Random(10).toString()
+        val response = makePostRequest(endpoint = "/api/v1/machines", payload = mapOf(
+                "name" to "Example machine $randomMachine",
+                "address" to "1.1.1.$randomMachine"))
+        print(response)
     }
 }
