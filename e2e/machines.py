155134ba4a6b132c03c348a5d48b1b58de286911
Nex Sabre <NexSabre@users.noreply.github.com>
2020-10-31 16:14:30 +0100

    .

--- e2e/machines.py
+++ e2e/machines.py
@@ -4,6 +4,15 @@ import requests
 import configuration as c
 
 
+def get_machine(machine_id):
+    r = requests.get(c.api_v1(f"/api/v1/machine/{machine_id}"))
+    try:
+        return json.loads(r.content)
+    except Exception as e:
+        print(e)
+        return {}
+
+
 def get_all_machines():
     content = requests.get(c.api_v1("/machines")).content
     return json.loads(content)
@@ -22,12 +31,20 @@ def add_example_machine():
         return {}
 
 
+def add_and_verifies_new_machine():
+    l_before = count_machines()
+    example_machine = add_example_machine()
+    l_after = count_machines()
+    assert l_after > l_before, "One machine should be added"
+    return example_machine
+
+
 def count_machines() -> int:
     return len(get_all_machines())
 
 
-def remove_specific_machine(id):
-    r = requests.get(c.api_v1(f"/machines/{id}/delete"))
+def remove_specific_machine(machine_id):
+    r = requests.get(c.api_v1(f"/machines/{machine_id}/delete"))
     return True if r.status_code == 204 else False
 
 
--- e2e/test_machines.py
+++ e2e/test_machines.py
@@ -5,18 +5,16 @@ from machines import *
 
 class TestMachines(unittest.TestCase):
     def test_add_one_machine(self):
-        l_before = count_machines()
-        add_example_machine()
-        l_after = count_machines()
+        add_and_verifies_new_machine()
 
-        self.assertGreater(l_after, l_before, "One machine should be added")
+    def test_add_one_machine_and_get_details(self):
+        created_machine = add_and_verifies_new_machine()
+        assert get_machine(created_machine["id"]), "Newly created machine should return details"
 
     def test_delete_one_machine(self):
         l_before = count_machines()
-        created_machine = add_example_machine()
+        created_machine = add_and_verifies_new_machine()
 
-        l_after_add = count_machines()
-        self.assertGreater(l_after_add, l_before)
         assert remove_specific_machine(created_machine["id"]), "Service should return No Content"
         self.assertEqual(l_before, count_machines(), "Machine should be removed")
 
