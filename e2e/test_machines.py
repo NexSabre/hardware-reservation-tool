3bd478d378fe59bcfddcd3b726a043106404cdf7
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-03 19:28:06 +0100

    Add setUp

--- e2e/test_machines.py
+++ e2e/test_machines.py
@@ -4,14 +4,16 @@ from machines import *
 
 
 class TestMachines(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_machine = add_and_verifies_new_machine()
+
     def test_add_one_machine(self):
         add_and_verifies_new_machine()
 
     def test_add_one_machine_and_get_details(self):
-        created_machine = add_and_verifies_new_machine()
-        machines_response = get_machine(created_machine["id"])
-        self.assertEqual(machines_response["address"], created_machine["address"])
-        self.assertEqual(machines_response["id"], created_machine["id"])
+        machines_response = get_machine(self.test_machine["id"])
+        self.assertEqual(machines_response["address"], self.test_machine["address"])
+        self.assertEqual(machines_response["id"], self.test_machine["id"])
 
     def test_delete_one_machine(self):
         l_before = count_machines()
@@ -21,20 +23,20 @@ class TestMachines(unittest.TestCase):
         self.assertEqual(l_before, count_machines(), "Machine should be removed")
 
     def test_after_creation_machine_should_be_enabled(self):
-        test_machine = get_machine(add_example_machine().get("id"))
+        test_machine = get_machine(self.test_machine["id"])
         assert test_machine.get("enabled"), "Machine should be default enabled"
 
     def test_after_creation_machine_try_to_enable_twice(self):
-        test_machine = get_machine(add_example_machine().get("id"))
-        self.assertTrue(test_machine.get("enabled"), "Machine by the default should be enabled: True")
-        self.assertTrue(enable(test_machine.get("id")), "Machine should return Ok, even if is in this state")
-        self.assertTrue(get_machine(test_machine.get("id")).get("enabled"), "Machine should be enabled")
+        test_machine = get_machine(self.test_machine["id"])
+        self.assertTrue(test_machine["enabled"], "Machine by the default should be enabled: True")
+        self.assertTrue(enable(test_machine["id"]), "Machine should return Ok, even if is in this state")
+        self.assertTrue(get_machine(test_machine["id"]).get("enabled"), "Machine should be enabled")
 
     def test_after_creation_machine_try_disable(self):
-        test_machine = get_machine(add_example_machine().get("id"))
-        self.assertTrue(test_machine.get("enabled"), "Machine by the default should be enabled: True")
-        self.assertTrue(disable(test_machine.get("id")), "Machine should return Ok, even if is in this state")
-        self.assertFalse(get_machine(test_machine.get("id")).get("enabled"), "Machine should be disabled")
+        test_machine = get_machine(self.test_machine["id"])
+        self.assertTrue(test_machine["enabled"], "Machine by the default should be enabled: True")
+        self.assertTrue(disable(test_machine["id"]), "Machine should return Ok, even if is in this state")
+        self.assertFalse(get_machine(test_machine["id"]).get("enabled"), "Machine should be disabled")
 
     def tearDown(self) -> None:
         remove_all_machines()
