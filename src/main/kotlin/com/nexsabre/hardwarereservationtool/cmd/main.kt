5bab095d0504efa999f670abce831cdeb4a86e73
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-06 09:34:46 +0100

    Small code refactor

--- src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/main.kt
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/main.kt
@@ -1,8 +1,13 @@
 package com.nexsabre.hardwarereservationtool.cmd
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
 import com.nexsabre.hardwarereservationtool.cmd.models.Hart
 
+
+class HartCmd : CliktCommand() {
+    override fun run() = Unit
+}
 
 class Info : CliktCommand() {
     override fun run() {
@@ -10,9 +15,24 @@ class Info : CliktCommand() {
     }
 }
 
+class Machines : CliktCommand() {
+    val helpMessage = "Help message"
+    override fun getFormattedHelp(): String {
+        return "asdasdasdasd"
+    }
+
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
 
 fun main(array: Array<String>) {
-    Info().main(array)
+    HartCmd()
+            .subcommands(
+                    Info(),
+                    Machines()
+            )
+            .main(array)
 }
 
 //* main
