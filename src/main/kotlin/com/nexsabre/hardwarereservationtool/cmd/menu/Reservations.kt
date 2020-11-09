1665bc5ef51575801b1bfaa5832e04cf1c7ba28a
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-09 22:21:51 +0100

    Move a subcommands to the separate directory

---
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/menu/Reservations.kt
@@ -0,0 +1,9 @@
+package com.nexsabre.hardwarereservationtool.cmd.menu
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class Reservations : CliktCommand() {
+    override fun run() {
+
+    }
+}
---
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/menu/Status.kt
@@ -0,0 +1,9 @@
+package com.nexsabre.hardwarereservationtool.cmd.menu
+
+import com.github.ajalt.clikt.core.CliktCommand
+
+class Status : CliktCommand() {
+    override fun run() {
+        TODO("Not yet implemented")
+    }
+}
