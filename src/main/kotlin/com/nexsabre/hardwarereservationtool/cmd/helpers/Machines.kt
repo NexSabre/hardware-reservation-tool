b4523754e03c68ca125c91e58f7626e2e81c70d7
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-11 11:02:45 +0100

    Move a .prettyPrint() extension func

--- src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/helpers/Machines.kt
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/helpers/Machines.kt
@@ -25,22 +25,3 @@ fun getAllMachines(): List<Element>? {
     val response = Hart().machines() ?: return null
     return Json.decodeFromString<List<Element>>(response)
 }
-
-fun List<Element>?.prettyPrint() {
-    if (this == null || this.isEmpty()) {
-        println("0. \t Nothing to display")
-
-    } else {
-        this.forEachIndexed { index, element ->
-            println("${index + 1}. $element")
-        }
-    }
-}
-
-fun Element?.prettyPrint() {
-    if (this == null) {
-        println("0. \t Nothing to display")
-    } else {
-        println("1. \t $this")
-    }
-}
