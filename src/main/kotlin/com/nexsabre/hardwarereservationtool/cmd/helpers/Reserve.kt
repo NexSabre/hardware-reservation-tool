e48e5baf9704296c34b5cdd5e0255cce2361bc8c
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-11 10:40:24 +0100

    Request and Release of reservation work

--- src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/helpers/Reserve.kt
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/helpers/Reserve.kt
@@ -3,12 +3,22 @@ package com.nexsabre.hardwarereservationtool.cmd.helpers
 import com.nexsabre.hardwarereservationtool.cmd.models.Hart
 
 fun reserve(machineId: Int): Boolean {
-    val response = Hart().reserve(machineId) ?: return false
+    val operation = Hart().reserve(machineId) ?: return false
+    if (!operation) {
+        println("Reservation of id: $machineId failed")
+        return false
+    }
+    println("Reservation of id: $machineId success")
     return true
 }
 
 fun release(machineId: Int): Boolean {
-    val response = Hart().release(machineId) ?: return false
+    val operationStatus = Hart().release(machineId) ?: return false
+    if (!operationStatus) {
+        println("Release of id: $machineId failed")
+        return false
+    }
+    println("Release of id: $machineId success")
     return true
 }
 
--- src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/models/Hart.kt
+++ src/main/kotlin/com/nexsabre/hardwarereservationtool/cmd/models/Hart.kt
@@ -4,12 +4,8 @@ import com.github.kittinunf.fuel.core.extensions.jsonBody
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.fuel.httpPost
 import com.nexsabre.hardwarereservationtool.cmd.data.ErrorData
-import kotlinx.serialization.Serializable
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
-
-@Serializable
-data class ReservationRequest(val id: Int, val duration: Int)
 
 
 class Hart {
@@ -28,6 +24,17 @@ class Hart {
     private fun getStatusCode(string: String): Int {
         val response = string.httpGet().responseString().second
         return response.statusCode
+    }
+
+    private fun getStatusCodeAndValidate(string: String,
+                                         success: List<Int> = listOf(200..299).flatten(),
+                                         fail: List<Int> = listOf(400..499).flatten()): Boolean? {
+        val response = string.httpGet().responseString().second
+        return when (response.statusCode) {
+            in success -> true
+            in fail -> false
+            else -> null
+        }
     }
 
     private fun getResultsOrNull(string: String): String? {
@@ -54,23 +61,20 @@ class Hart {
         return true
     }
 
-    fun reserve(machineId: Int): Boolean? {
-        val obj = ReservationRequest(machineId, 2)
-        val json = Json.encodeToString<ReservationRequest>(obj)
-        val ss = mapOf(
-                "json" to "something",
-                "asdfd" to "asdfasdf"
+    fun reserve(machineId: Int, duration: Int = 0): Boolean? {
+        val requestValue = mapOf(
+                "id" to machineId,
+                "duration" to duration
         )
-        postStatusCode(
+        val jsonBody = Json.encodeToString(requestValue)
+        return postStatusCode(
                 Endpoints().reserveMachineId(machineId),
-                json
+                jsonBody
         )
-        return true
     }
 
     fun release(machineId: Int): Boolean? {
-        getStatusCode(Endpoints().releaseMachineId(machineId))
-        return true
+        return getStatusCodeAndValidate(Endpoints().releaseMachineId(machineId))
     }
 
 }
