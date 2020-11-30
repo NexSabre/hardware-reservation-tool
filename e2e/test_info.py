4a4ca77a87fc9a198d6f27bb4603e33c6c9ffe54
Nex Sabre <NexSabre@users.noreply.github.com>
2020-11-30 20:16:13 +0100

    fix typo

--- e2e/test_info.py
+++ e2e/test_info.py
@@ -13,7 +13,6 @@ class TestInfo(unittest.TestCase):
     def test_info_should_return_200(self):
         assert self.response.status_code == 200, "Should return a 200"
         
-    def test_info_should_conatain_version(self):
+    def test_info_should_contain_version(self):
         response_body = json.loads(self.response.content)
         self.assertTrue(response_body.get("version", False), "Key version should exist")
-       
