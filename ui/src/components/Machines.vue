2c1204c749ea1004be065db24f7ca4a11d9c0f4e
Nex Sabre <NexSabre@users.noreply.github.com>
2021-01-23 16:43:22 +0100

    Initial version of web ui

---
+++ ui/src/components/Machines.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+        Count machines: {{ allMachines }} <br>
+       {{ machines }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: "Machines",
+    data() {
+        return {
+            machines: []
+        }
+    },
+    methods: {
+        getMachines() {
+            const baseURI = "http://localhost:8080/api/v1/machines"
+            this.$http.get(baseURI)
+                .then(result => this.machines = result.data)
+        }
+    }, 
+    mounted() {
+        this.getMachines()
+    }
+}
+</script>
+
+<style>
+
+</style>
